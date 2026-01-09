16cb3aebbfbc1f9fa897f1b744c0ae5ad63dc9e7
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-09 23:36:58 +0100

    fix

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,20 +22,6 @@ require("lazy").setup({
   },
 
 
-  -- Treesitter
-  { 
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
-    event = "BufReadPost",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = "all",
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  },
-
   -- Autopairs
   {
     "windwp/nvim-autopairs",
@@ -58,54 +44,6 @@ require("lazy").setup({
       require("telescope").setup({})
     end,
   },
-
-  -- LSP + completion
-  {
-    "neovim/nvim-lspconfig",
-    event = "BufReadPre",
-    config = function()
-      local lsp = vim.lsp
-  
-      -- Lua
-      if lsp.config["lua_ls"] then
-        lsp.config["lua_ls"].setup({})
-      end
-  
-      -- TypeScript
-      if lsp.config["ts_ls"] then
-        lsp.config["ts_ls"].setup({})
-      end
-  
-      -- Python
-      if lsp.config["pyright"] then
-        lsp.config["pyright"].setup({})
-      end
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-        },
-      })
-    end,
-  }
 },
 { 
   lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
