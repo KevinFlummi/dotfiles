20e99cab140a9aee61e27a395cfe4caf55078db3
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-10 00:34:17 +0100

    formatter

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,7 @@ local set = vim.opt
 
 -- basic
 set.title = true
-set.background="dark"
+set.background = "dark"
 set.mouse = a
 set.cursorline = true
 set.hidden = true
@@ -16,8 +16,8 @@ set.relativenumber = true
 set.wildmenu = true
 set.clipboard:append("unnamedplus")
 
-set.backspace="indent,eol,start"
-set.scrolloff=8
+set.backspace = "indent,eol,start"
+set.scrolloff = 8
 
 -- tab behaviour
 set.expandtab = true
@@ -25,10 +25,9 @@ set.autoindent = true
 set.shiftwidth = 4
 set.tabstop = 4
 
-
 -- terminal colors
 set.termguicolors = true
-set.colorcolumn="80"
+set.colorcolumn = "80"
 
 -- faster cursor hold
 set.updatetime = 50
@@ -38,14 +37,14 @@ set.updatetime = 50
 ---------------------------------------------------
 local map = vim.keymap.set
 
-vim.g.mapleader=","
+vim.g.mapleader = ","
 map("n", "ü", "{")
 map("n", "ä", "{")
 map("v", "ü", "{")
 map("v", "ä", "{")
 if vim.fn.exists(":Telescope") == 2 then
-    map("n", "<leader>ff", "<cmd>Telescope find_files<cr>") 
-    map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>") 
-    map("n", "<leader>fb", "<cmd>Telescope buffers<cr>") 
-    map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+	map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+	map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+	map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+	map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 end
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,125 +1,114 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-  -- Statusline
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require("lualine").setup({
-        options = { theme = "auto" },
-      })
-    end,
-  },
+	-- Statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = { theme = "auto" },
+			})
+		end,
+	},
 
+	-- Autopairs
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
 
-  -- Autopairs
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  },
+	-- Colorizer
+	{
+		"NvChad/nvim-colorizer.lua",
+	},
 
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({})
+		end,
+	},
 
-  -- Colorizer
-  { 
-    "NvChad/nvim-colorizer.lua" 
-  },
+	-- Formatting
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				-- Python
+				python = { "ruff_format" },
 
-  -- Telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({})
-    end,
-  },
+				-- Nix
+				nix = { "alejandra" },
 
-  -- Formatting
-{
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      -- Python
-      python = { "ruff_format" },
+				-- Shell
+				sh = { "shfmt" },
+				bash = { "shfmt" },
 
-      -- Nix
-      nix = { "alejandra" },
+				-- Lua
+				lua = { "stylua" },
 
-      -- Shell
-      sh = { "shfmt" },
-      bash = { "shfmt" },
+				-- TOML
+				toml = { "taplo" },
 
-      -- Lua
-      lua = { "stylua" },
+				-- JSON / JSONC
+				json = { "prettier" },
+				jsonc = { "prettier" },
 
-      -- JSON / JSONC
-      json = { "prettier" },
-      jsonc = { "prettier" },
+				-- YAML
+				yaml = { "prettier" },
 
-      -- YAML
-      yaml = { "prettier" },
+				-- Markdown
+				markdown = { "prettier" },
 
-      -- TOML
-      toml = { "taplo" },
+				-- HTML
+				html = { "prettier" },
 
-      -- Markdown
-      markdown = { "prettier" },
+				-- JavaScript / TypeScript
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
 
-      -- HTML / XML
-      html = { "prettier" },
-      xml = { "tidy" },
+				-- Rust
+				rust = { "rustfmt" },
 
-      -- JavaScript / TypeScript
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
+				-- Go
+				go = { "gofmt" },
 
-      -- Rust
-      rust = { "rustfmt" },
+				-- C / C++ / Java / ObjC
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+				java = { "clang_format" },
 
-      -- Go
-      go = { "gofmt" },
+				-- Docker
+				dockerfile = { "dockfmt" },
 
-      -- C / C++ / Java / ObjC
-      c = { "clang_format" },
-      cpp = { "clang_format" },
-      java = { "clang_format" },
+				-- SQL
+				sql = { "sqlfluff" },
+			},
 
-      -- Docker
-      dockerfile = { "dockfmt" },
-
-      -- Terraform / HCL
-      terraform = { "terraform_fmt" },
-      hcl = { "terraform_fmt" },
-
-      -- SQL
-      sql = { "sqlfluff", },
-
-      -- Protobuf
-      proto = { "buf" },
-    },
-
-    format_on_save = {
-      timeout_ms = 800,
-      lsp_fallback = true,
-    },
-  },
-}
-
-},
-{ 
-  lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
+			format_on_save = {
+				timeout_ms = 800,
+				lsp_fallback = true,
+			},
+		},
+	},
+}, {
+	lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
 })
-
