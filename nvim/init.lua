0c78fb335b862ae2bb1e954a9bfa88c5c0b3a54c
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-09 22:34:28 +0100

    lua syntax

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,29 +6,32 @@ require("plugins")
 local set = vim.opt
 
 -- basic
-set.title
-set.background=dark
-set.mouse=a
-set.cursorline
-set.hidden
-set.number
-set.relativenumber
-set.wildmenu
-set.clipboard+=unnamedplus
-set.completeopt=noinsert,menuone,noselect
+set.title = true
+set.background="dark"
+set.mouse = a
+set.cursorline = true
+set.hidden = true
+set.number = true
+set.relativenumber = true
+set.wildmenu = true
+set.clipboard:append("unnamedplus")
+
+set.backspace="indent,eol,start"
+set scrolloff=8
 
 -- tab behaviour
-set.expandtab
-set.autoindent
-set.shiftwidth=4
-set.tabstop=4
+set.expandtab = true
+set.autoindent = true
+set.shiftwidth = 4
+set.tabstop = 4
 
 
 -- terminal colors
-set.termguicolors
-set.colorcolumn=80
+set.termguicolors = true
+set.colorcolumn="80"
 
-set scrolloff=8
+-- faster cursor hold
+set.updatetime = 50
 
 ---------------------------------------------------
 ---- Keybinds
