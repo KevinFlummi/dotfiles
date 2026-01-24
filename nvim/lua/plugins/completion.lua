c5d697f111cf05e6edc2a059728515634a9644d1
Kevin Ferneding <email@kevinferneding.xyz>
2026-01-24 16:09:58 +0100

    copilot into lsp

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,27 +1,28 @@
 require("blink.cmp").setup({
-    sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-    },
+	sources = {
+		default = { "copilot", "lsp", "path", "snippets", "buffer" },
+	},
 
-    keymap = {
-        -- Preset: Use no presets
-        preset = 'default',
-        -- These are the defaults: 
-        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<C-e>'] = { 'hide', 'fallback' },
-        ['<C-y>'] = { 'select_and_accept', 'fallback' },
-        
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-        
-        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-        
-        ['<Tab>'] = { 'snippet_forward', 'fallback' },
-        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-        
-        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' }, 
-    },
+	keymap = {
+		-- Preset: Use no presets
+		preset = "default",
+		-- These are the defaults:
+		["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+		["<C-e>"] = { "hide", "fallback" },
+		["<C-y>"] = { "select_and_accept", "fallback" },
+
+		["<Up>"] = { "select_prev", "fallback" },
+		["<Down>"] = { "select_next", "fallback" },
+		["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+		["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+		["<C-b>"] = { "scroll_documentation_up", "fallback" },
+		["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+		["<Tab>"] = { "snippet_forward", "fallback" },
+		["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+		["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+		--]]
+	},
 })
